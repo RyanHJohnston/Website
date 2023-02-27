db274049b49be77fabb7e7eb3cf32a314d9417b2
Ryan Johnston <ryanhjohnstoncollege@gmail.com>
2023-02-27 04:03:28 +0000

    Fixed shit

---
+++ main.js
@@ -0,0 +1,18 @@
+firstName = "Ryan";
+lastName = "Johnston";
+age = 21;
+
+
+/**
+ * 
+ *
+ * @param {[TODO:type]} firstName - [TODO:description]
+ * @param {[TODO:type]} lastName - [TODO:description]
+ * @param {[TODO:type]} age - [TODO:description]
+ */
+function helloWorld(firstName, lastName, age) {
+  console.log("Hello, my name is " + firstName + " " 
+    + lastName + " and I am " + age + "  years old.");   
+}
+
+
